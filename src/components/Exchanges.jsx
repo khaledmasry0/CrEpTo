922e87f4f6f26754f4154448e9e77a645a6fcae2
Masry <khaled.m00@yahoo.com>
2022-12-22 21:54:03 +0200

    first commit

---
+++ src/components/Exchanges.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// unfortunately this api need a premium plan so i won't do anything to this component
+
+const Exchanges = () => {
+  return <div>Exchanges</div>;
+};
+
+export default Exchanges;
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,53 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "X-RapidAPI-Key": "b96a49e8efmsh5707a394f255e60p1af7efjsn33e653b03edf",
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`),
+    }),
+    getCryptoHistory: builder.query({
+      query: ({ coinId, timePeriod }) =>
+        createRequest(`coin/${coinId}/history?timePeriod=${timePeriod}`),
+    }),
+    getExchanges: builder.query({
+      query: () => createRequest("/exchanges"),
+    }),
+  }),
+});
+
+export const {
+  useGetCryptosQuery,
+  useGetCryptoDetailsQuery,
+  useGetExchangesQuery,
+  useGetCryptoHistoryQuery,
+} = cryptoApi;
+// const options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/coins',
+//     params: {
+//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//       timePeriod: '24h',
+//       'tiers[0]': '1',
+//       orderBy: 'marketCap',
+//       orderDirection: 'desc',
+//       limit: '50',
+//       offset: '0'
+//     },
+//     headers: {
+//       'X-RapidAPI-Key': 'b96a49e8efmsh5707a394f255e60p1af7efjsn33e653b03edf',
+//       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//     }
+//   };
